f24dc1eb5580f826ec5c899375672c05f78468ad
Cristian Falcone <cristianfalcone@hotmail.com>
2025-09-19 23:05:35 -0300

    TP2 UNTREF e-Commerce FINANCE

--- js/pago.js
+++ js/pago.js
@@ -3,10 +3,16 @@ import { traerProductos, urlCarro } from "./apis.js";
 const contTransferencia=document.getElementById('contenedorTransferencia');
 const contTarjeta=document.getElementById('contenedorTarjeta');
 
+const ventanaModal=document.getElementById('ventanaModal');
+const botonCerrarModal=document.getElementById('cerrarModal');
+const cuerpoModal=document.getElementById('cuerpoModal');
+
 const numeroTarjeta=document.getElementById('card-number');
 const nombreTarjeta=document.getElementById('card-name');
 const vencTarjeta=document.getElementById('expiry-date');
 const codigoCVV=document.getElementById('cvv');
+const confirmarPago=document.getElementById('botonPagar');
+let pagoCon="";
 
 
 async function mostrarDetalleInversiones(){
@@ -120,6 +126,84 @@ function habilitarEstadoModosPago(){
 
 }
 
+function controlCamposTarjeta(){
+  numeroTarjeta.addEventListener('input',()=>{
+        console.log(numeroTarjeta.value);
+        let caracter=""
+        let datoIngresado="";
+        for (let i = 0; i < numeroTarjeta.value.length; i=i+1) {
+                caracter=numeroTarjeta.value[i];
+                if ((caracter==="0")||(caracter==="1")||(caracter==="2")||(caracter==="3")|| (caracter==="4")|| (caracter==="5")|| (caracter==="6")|| (caracter==="7")|| (caracter==="8")|| (caracter==="9")|| (caracter===" ")){
+                                        datoIngresado=datoIngresado+caracter;
+                                                                           }   
+        }
+        numeroTarjeta.value=datoIngresado;
+        console.log(datoIngresado);
+  });
+  vencTarjeta.addEventListener('input',()=>{
+        console.log(vencTarjeta.value);
+        let caracter=""
+        let datoIngresado="";
+        for (let i = 0; i < vencTarjeta.value.length; i=i+1) {
+                caracter=vencTarjeta.value[i];
+                if ((caracter==="0")||(caracter==="1")||(caracter==="2")||(caracter==="3")|| (caracter==="4")|| (caracter==="5")|| (caracter==="6")|| (caracter==="7")|| (caracter==="8")|| (caracter==="9")|| (caracter==="/")){
+                                        datoIngresado=datoIngresado+caracter;
+                                                                           }   
+        }
+        vencTarjeta.value=datoIngresado;
+        console.log(datoIngresado);
+  })  
+  codigoCVV.addEventListener('input',()=>{
+        console.log(codigoCVV.value);
+        let caracter=""
+        let datoIngresado="";
+        for (let i = 0; i < codigoCVV.value.length; i=i+1) {
+                caracter=codigoCVV.value[i];
+                if ((caracter==="0")||(caracter==="1")||(caracter==="2")||(caracter==="3")|| (caracter==="4")|| (caracter==="5")|| (caracter==="6")|| (caracter==="7")|| (caracter==="8")|| (caracter==="9")){
+                                        datoIngresado=datoIngresado+caracter;
+                                                                           }   
+        }
+        codigoCVV.value=datoIngresado;
+        console.log(datoIngresado);
+  })
+}
+
+function mostrarValidacionTarjeta(){
+   botonCerrarModal.style.display="none";
+   ventanaModal.showModal();
+   const textoModal=document.getElementById('textoModal');
+   const mensaje=document.createElement('p');
+   mensaje.setAttribute('id','mensaje');
+   mensaje.textContent="Estamos validando tus datos...";
+   textoModal.appendChild(mensaje);
+   setTimeout(()=>{
+        const azar=Math.floor(Math.random() * (1000 + 1));
+        console.log(azar);
+        azar>500?mensaje.textContent="TARJETA APROBADA":mensaje.textContent="TARJETA INVALIDA"; 
+        botonCerrarModal.style.display="inline-block";       
+   },2000)
+   botonCerrarModal.addEventListener('click',()=>{
+       textoModal.textContent="";
+       console.log('click en boton CERRAR MODAL');
+       ventanaModal.close();
+   })
+}
+
+function mostrarValidacionTransferencia(){
+   ventanaModal.showModal();
+   const textoModal=document.getElementById('textoModal');
+   const mensaje=document.createElement('p');
+   mensaje.textContent="Estaremos esperando tu transferencia.";
+   textoModal.appendChild(mensaje);
+   botonCerrarModal.addEventListener('click',()=>{
+       textoModal.textContent="";
+       console.log('click en boton CERRAR MODAL');
+       ventanaModal.close();
+   })
+
+}
+
+
 mostrarDetalleInversiones();
 deshabilitarEstadoModosPago();
 
@@ -129,9 +213,10 @@ contTarjeta.addEventListener('click',(evento)=>{
     contTransferencia.style.color="grey";
     contTarjeta.style.border="rgb(223, 45, 178) solid 5px";
     contTarjeta.style.color="black"
+    pagoCon="TARJETA";
+    controlCamposTarjeta();
     console.log("TARJETA");
     console.log(evento.currentTarget.id);
-
 });
 
 contTransferencia.addEventListener('click',(evento)=>{
@@ -140,7 +225,25 @@ contTransferencia.addEventListener('click',(evento)=>{
     contTarjeta.style.color="grey";
     contTransferencia.style.color="black";
     contTransferencia.style.border="rgb(223, 45, 178) solid 5px";
+    pagoCon="TRANSFERENCIA";
     console.log("TRANSFERENCIA");
     console.log(evento.currentTarget.id);
+});
+
+confirmarPago.addEventListener('click',()=>{
+    if (pagoCon==="TARJETA"){
+           if ((numeroTarjeta.value.length===19) && (nombreTarjeta.value!="") && (vencTarjeta.value.length===5) && (codigoCVV.value.length===3)){
+                                       mostrarValidacionTarjeta();
+                                                                  } else alert("Tarjeta con datos incompletos!");
+           console.log("Presiono Boton Confirmar Pago...")
+           console.log("CONTROLAR TARJETA");
+           console.log("Numero: ",numeroTarjeta.value," Nombre: ", nombreTarjeta.value, " Venc.: ", vencTarjeta.value, " CODIGO: ",codigoCVV.value);
+      }
+
+    if (pagoCon==="TRANSFERENCIA"){
+           mostrarValidacionTransferencia();
+           console.log("Presiono Boton Confirmar Pago...")
+           console.log("CONTROLAR TRANSFERENCIA");
+      }
 });
 
